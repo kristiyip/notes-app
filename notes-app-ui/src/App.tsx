f69087999119b728b8ed06a3849b214aa30f5f6c
Kristin Diep <kristiyip@gmail.com>
2024-10-04 14:19:59 -0400

    create controlled inputs and handle form submission

--- notes-app-ui/src/App.tsx
+++ notes-app-ui/src/App.tsx
@@ -1,14 +1,67 @@
+import { useState } from "react";
 import "./App.css"
 
+type Note = {
+  id: number;
+  title: string;
+  content: string;
+}
+
 export default function App() {
+  const [notes, setNotes] = useState<Note[]>([
+    {
+      id: 0,
+      title: "title 0",
+      content: "content 0"
+    },
+    {
+      id: 1,
+      title: "title 1",
+      content: "content 1"
+    },
+    {
+      id: 2,
+      title: "title 2",
+      content: "content 2"
+    },
+    {
+      id: 3,
+      title: "title 3",
+      content: "content 3"
+    }
+  ])
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleSubmit = (
+    event: React.FormEvent
+  ) => {
+    event.preventDefault();
+    const newNote: Note = {
+      id: notes.length + 1,
+      title: title,
+      content: content
+    }
+    setNotes([newNote, ...notes])
+    setTitle("")
+    setContent("")
+  }
+
   return (
     <div className="app-container">
-      <form className="note-form">
+      <form 
+        className="note-form"
+        onSubmit={(e) => handleSubmit(e)}
+      >
         <input 
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
           placeholder="title"
           required
         />
         <textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
           placeholder="Content"
           rows={10}
           required
@@ -18,13 +71,15 @@ export default function App() {
         </button>
       </form>
       <div className="notes-grid">
-        <div className="note-item">
-          <div className="notes-header">
-            <button>x</button>
+        {notes.map((note) => (
+          <div className="note-item">
+            <div className="notes-header">
+              <button>x</button>
+            </div>
+            <h2>{note.title}</h2>
+            <p>{note.content}</p>
           </div>
-          <h2>Note Title</h2>
-          <p>Note Content</p>
-        </div>
+        ))}
       </div>
     </div>
   )
