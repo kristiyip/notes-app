9ae18124e9c9ead207f714840ce6069c0f480bfb
Kristin Diep <kristiyip@gmail.com>
2024-10-09 21:23:04 -0400

    post newNote route

--- server/src/routes/notes.ts
+++ server/src/routes/notes.ts
@@ -4,7 +4,7 @@ import NoteModel from '../schema/note';
 const router = express.Router();
 
 //@ts-ignore
-router.get('/getAllNotes', async (req: Request, res: Response ) => {
+router.get('/getAllNotes', async (req: Request, res: Response) => {
   try {
     const notes = await NoteModel.find({})
 
@@ -16,5 +16,17 @@ router.get('/getAllNotes', async (req: Request, res: Response ) => {
     res.status(500).send(err);
   }
 });
+
+router.post('/', async (req: Request, res: Response) => {
+  try {
+    const newNoteBody = req.body
+    const newNote = new NoteModel(newNoteBody)
+    const savedNote = await newNote.save()
+
+    res.status(200).send(savedNote)
+  } catch (err) {
+    res.status(500).send(err);
+  }
+})
 
 export default router;
