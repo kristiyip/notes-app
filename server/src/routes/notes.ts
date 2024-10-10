433fd5e7622a54ff6b467aa947c14b8d74d1f4a6
Kristin Diep <kristiyip@gmail.com>
2024-10-09 21:31:42 -0400

    created route for updating a note

--- server/src/routes/notes.ts
+++ server/src/routes/notes.ts
@@ -4,6 +4,7 @@ import NoteModel from '../schema/note';
 const router = express.Router();
 
 //@ts-ignore
+// Route for getting all notes in db
 router.get('/getAllNotes', async (req: Request, res: Response) => {
   try {
     const notes = await NoteModel.find({})
@@ -17,6 +18,7 @@ router.get('/getAllNotes', async (req: Request, res: Response) => {
   }
 });
 
+// Route for adding a new note to db
 router.post('/', async (req: Request, res: Response) => {
   try {
     const newNoteBody = req.body
@@ -24,6 +26,26 @@ router.post('/', async (req: Request, res: Response) => {
     const savedNote = await newNote.save()
 
     res.status(200).send(savedNote)
+  } catch (err) {
+    res.status(500).send(err);
+  }
+})
+
+//@ts-ignore
+// Route for updating a note in db
+router.put('/:id', async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const newNoteBody = req.body;
+    const note = await NoteModel.findByIdAndUpdate(
+      id,
+      newNoteBody,
+      { new: true }
+    );
+
+    if(!note) return res.status(404).send();
+
+    res.status(200).send(note)
   } catch (err) {
     res.status(500).send(err);
   }
