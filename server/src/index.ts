c63432bc2dde47309a5e9a00478139a1a282faf2
Kristin Diep <kristiyip@gmail.com>
2024-10-09 20:43:59 -0400

    setup mongodb and mongoose

---
+++ server/src/index.ts
@@ -0,0 +1,21 @@
+import express, { Express } from 'express';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+
+const app: Express = express();
+const port = process.env.PORT || 3001
+
+dotenv.config()
+
+app.use(express.json());
+
+const mongoURI: string = process.env.MONGOURI || ""
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("Connected to MongoDB!"))
+  .catch((err) => console.error("Failed to connect to MongoDB: ", err))
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
